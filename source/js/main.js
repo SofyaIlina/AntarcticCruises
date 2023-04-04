f6c6882ea335f7267075cccd9371c77666629b67
SofyaIlina <kenny8556@yandex.ru>
2023-04-05 03:00:26 +0500

    правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initMap} from './modules/map';
 
 // ---------------------------------
 
@@ -17,6 +18,36 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const map = document.getElementById('map');
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        initMap({
+          id: 'map',
+          initials: {
+            center: [59.9387165, 30.3230474],
+            controls: [],
+            zoom: 16,
+          },
+          placemark: [
+            {
+              hintContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8',
+            },
+            {
+              iconImageHref: 'img/sprite/map-mark.svg',
+              iconImageSize: [18, 22],
+              iconLayout: 'default#image',
+              iconShadow: false,
+            }
+          ],
+        });
+        observer.unobserve(map);
+      }
+    }, {
+      rootMargin: '0px',
+      threshold: 0,
+    });
+
+    observer.observe(map);
     initModals();
     const form = new Form();
     window.form = form;
